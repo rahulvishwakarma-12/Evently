4ca65d36b47c137d216f040834b4e2cffc6058ae
Rahul Vishwakarma <rahulvishwakarma88872@gmail.com>
2024-12-15 13:29:39 +0530

    V2

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,34 +3,41 @@ import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
 import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
-    return (
-      <header className="w-full border-b">
-        <div className="wrapper flex items-center justify-between ">
-          <Link href="/" className="w-36">
-            <Image 
-              src="/assets/images/logo.svg" width={128} height={38}
-              alt="Evently logo" 
-            />
-          </Link>
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image 
+            src="/assets/images/logo.svg" width={128} height={38}
+            alt="Evently logo" 
+          />
+        </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3">
           <SignedIn>
-            <UserButton/>
-            <NavItems/>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
-          <div className="flex w-32 justify-end gap-3">
-            <SignedOut>
-              <Button asChild className="rounded-full" size="lg">
-                <Link href="/sign-in">
-                  Login
-                </Link>
-              </Button>
-              <NavItems/>
-            </SignedOut>
-          </div>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
         </div>
-      </header>
-    )
-  }
-  
-  export default Header
+      </div>
+    </header>
+  )
+}
+
+export default Header
