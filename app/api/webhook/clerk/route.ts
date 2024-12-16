183b05a3eca2ed5cca157b00ba3fac5a89571637
Rahul Vishwakarma <rahulvishwakarma88872@gmail.com>
2024-12-16 12:38:15 +0530

    Revert "success"
    This reverts commit 394ab2e9c45afb4620fff9494379474778d53ce6.

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -3,7 +3,9 @@ import { headers } from 'next/headers'
 import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, updateUser } from '@/lib/actions/user.actions'
 import { NextResponse } from 'next/server'
-import { createUser, updateUser, deleteUser } from '@/lib/actions/user.actions'
+
+
+
 
 export async function POST(req: Request) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET
@@ -52,26 +54,27 @@ export async function POST(req: Request) {
   // For this guide, log payload to console
   const { id } = evt.data
   const eventType = evt.type
-
+  
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username,
-      firstName: first_name,
-      photo: image_url,
-    }
+      username: username!,
+      firstName: first_name ?? '',
+      lastName: last_name ?? '',
+      photo: image_url
+    };
 
     const newUser = await createUser(user);
-
+    
     if(newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser._id
-        }
-      })
+      // await clerkClient.users.updateUserMetadata(id, {
+      //   publicMetadata: {
+      //     userId: newUser._id
+      //   }
+      // })
     }
 
     return NextResponse.json({ message: 'OK', user: newUser })
@@ -81,10 +84,10 @@ export async function POST(req: Request) {
     const {id, image_url, first_name, last_name, username } = evt.data
 
     const user = {
-      firstName: first_name!,
-      lastName: last_name!,
       username: username!,
-      photo: image_url,
+      firstName: first_name ?? '',
+      lastName: last_name ?? '',
+      photo: image_url
     }
 
     const updatedUser = await updateUser(id, user)
